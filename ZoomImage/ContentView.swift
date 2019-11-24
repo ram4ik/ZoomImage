6e6049da01e35b4fb07c1aac22bc5bb9e6941967
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-24 19:16:02 +0200

    init

--- ZoomImage/ContentView.swift
+++ ZoomImage/ContentView.swift
@@ -9,8 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var show = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image(uiImage: UIImage(named: "Garfield")!)
+                .resizable()
+                .scaledToFit()
+                .frame(width: show ? 40 : 250, height: show ? 100: 750)
+                .onTapGesture {
+                    withAnimation {
+                        self.show.toggle()
+                    }
+            }
+        }
     }
 }
 
